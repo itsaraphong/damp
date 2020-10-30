b7ab3df4055c69cb2aff6119f5fe6fddce74d46a
itsaraphong <73689894+itsaraphong@users.noreply.github.com>
2020-10-30 15:36:39 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello world</title>
+        </head>
+        <body>
+            <h1>อิสระพงษ์ วัชรประทีป</h1>
+            <p>ชื่อเล่น ดามพ์</p>
+            <img src="cat2.jpg">
+            <img src="cat.jpg">
+            <br>
+            <a href="http:/www.it.kmitl.ac.th">This is a link</a>
+
+            <h2>หล่อเท่สุดๆ</h2>
+            <ul>
+                <li>A: เธอที่มาชอบผม</li>
+                <li>B: ผมที่อยู่เฉยๆก็หล่อเท่ชิบหาย</li>
+            </ul>
+        </body>
+</html>
